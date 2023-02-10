4e27b3c3ae06999c8cf7955c82beaa23c83f2497
KvanQuilst <eskew.dylan@outlook.com>
2023-02-10 11:39:22 -0800

    checkpoint

--- sys/include/threads-manager.ads
+++ sys/include/threads-manager.ads
@@ -1,6 +1,6 @@
 --------------------------------------------------------------------------------
 --                                                                            --
--- threads.ads - LovelaceOS, Copyright 2023 - Dylan Eksew                     --
+-- threads-manager.ads - LovelaceOS, Copyright 2023 - Dylan Eksew             --
 --                                                                            --
 -- LovelaceOS is free software: you can redistribute it and/or modify         --
 -- it under the terms of the GNU General Public License as published by       --
@@ -16,29 +16,14 @@
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.      --
 --                                                                            --
 --------------------------------------------------------------------------------
-with Interfaces; use Interfaces;
-with Kernel; use Kernel;
+with Synchrony; use Synchrony;
 
-package Threads is
+package Threads.Manager is
 
-  type Start_Function is access procedure (InitArgs : Unsigned_64);
-  type Thread_State is (Idle, Ready, Running, Blocked)
-    with Size => 8;
+private
 
-  type Thread is record
-    ID           : Unsigned_8;
-    State        : Thread_State;
-    Thread_Start : Start_Function;
-    Init_Args    : Unsigned_64 := 0;
-  end record
-    with Pack;
+  type Thread_Manager is record
+    M : access Mutex;
+  end record;
 
-  type Thread_Acc is access Thread;
-
-  function Init return Thread;
-
-  procedure Run   (T : Thread_Acc);
-  procedure Yield (T : Thread_Acc);
-  procedure Sleep (T : Thread_Acc);
-
-end Threads;
+end Threads.Manager;
--- sys/kernel/threads-manager.adb
+++ sys/kernel/threads-manager.adb
@@ -1,6 +1,6 @@
 --------------------------------------------------------------------------------
 --                                                                            --
--- threads.ads - LovelaceOS, Copyright 2023 - Dylan Eksew                     --
+-- threads-manager.adb - LovelaceOS, Copyright 2023 - Dylan Eksew             --
 --                                                                            --
 -- LovelaceOS is free software: you can redistribute it and/or modify         --
 -- it under the terms of the GNU General Public License as published by       --
@@ -16,29 +16,7 @@
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.      --
 --                                                                            --
 --------------------------------------------------------------------------------
-with Interfaces; use Interfaces;
-with Kernel; use Kernel;
 
-package Threads is
+package body Threads.Manager is
 
-  type Start_Function is access procedure (InitArgs : Unsigned_64);
-  type Thread_State is (Idle, Ready, Running, Blocked)
-    with Size => 8;
-
-  type Thread is record
-    ID           : Unsigned_8;
-    State        : Thread_State;
-    Thread_Start : Start_Function;
-    Init_Args    : Unsigned_64 := 0;
-  end record
-    with Pack;
-
-  type Thread_Acc is access Thread;
-
-  function Init return Thread;
-
-  procedure Run   (T : Thread_Acc);
-  procedure Yield (T : Thread_Acc);
-  procedure Sleep (T : Thread_Acc);
-
-end Threads;
+end Threads.Manager;
